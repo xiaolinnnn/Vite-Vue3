c06e4e893ed31e29bab5af33f40ff48bf2d87f07
xlchen <2509060449@qq.com>
2022-06-22 14:44:40 +0800

    'ui组件搭建'

--- build/plugin/index.js
+++ build/plugin/index.js
@@ -8,12 +8,22 @@ import VueSetupExtend from 'vite-plugin-vue-setup-extend'
 import visualizer from 'rollup-plugin-visualizer'
 import { configHtmlPlugin } from './html'
 import { unocss } from './unocss'
+/**
+ * * 组件库按需引入插件
+ * usage: 直接使用组件,无需在任何地方导入组件
+ */
+import Components from 'unplugin-vue-components/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+
 export function createVitePlugins(viteEnv, isBuild) {
   const plugins = [
     vue(),
     VueSetupExtend(),
     configHtmlPlugin(viteEnv, isBuild),
     unocss(),
+    Components({
+      resolvers: [NaiveUiResolver()],
+    }),
   ]
   if (isBuild) {
     plugins.push(
